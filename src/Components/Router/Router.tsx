94776965993c5d667d650dda1c70ea29557321cf
katiacammisa <katia.cammisa@ing.austral.edu.ar>
2021-06-12 18:58:12 -0300

    Created Login

--- src/Components/Router/Router.tsx
+++ src/Components/Router/Router.tsx
@@ -1,10 +1,12 @@
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import MainPage from "../Main";
+import Login from "../Login/Login";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path={"/login"} exact component={Login}/>
         <Route path={"/"} exact component={MainPage}/>
       </Switch>
     </BrowserRouter>
